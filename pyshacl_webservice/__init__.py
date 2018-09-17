c42eff29d425299ba4efb9b1cacac8aca652fdfd
Ashley Sommer <ashleysommer@gmail.com>
2018-09-17 14:04:05 +1000

    Applied same template changes to Sanic app Added sanic-jinja2 dependency to sanic app, for rendering jinja templates Changed pyshacl requirement to the PyPI version, and bump to newer bugfix release.

--- pyshacl_webservice/__init__.py
+++ pyshacl_webservice/__init__.py
@@ -11,6 +11,6 @@ except (ImportError, RuntimeError):
     flask_app = None
 
 # version compliant with https://www.python.org/dev/peps/pep-0440/
-__version__ = '0.1.0a1.dev20180908'
+__version__ = '0.1.0a2.dev20180917'
 
 __all__ = ['flask_app', 'sanic_app', '__version__']
