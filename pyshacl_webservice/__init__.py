f9067fb9e2b95f0a93cec71cf00570a86312fd49
Ashley Sommer <ashleysommer@gmail.com>
2020-07-22 21:30:24 +1000

    bumped version

--- pyshacl_webservice/__init__.py
+++ pyshacl_webservice/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: latin-1 -*-
 import sys
-assert sys.version[0] == "3", "Please use python3"
-
+assert sys.version[0] == "3", "Please use python3.6+"
 try:
     from pyshacl_webservice.sanic_app import app as sanic_app
 except (ImportError, RuntimeError):
@@ -13,6 +12,6 @@ except (ImportError, RuntimeError):
     flask_app = None
 
 # version compliant with https://www.python.org/dev/peps/pep-0440/
-__version__ = '0.1.0a4.dev20181123'
+__version__ = '0.1.0a5.dev20200722'
 
 __all__ = ['flask_app', 'sanic_app', '__version__']
